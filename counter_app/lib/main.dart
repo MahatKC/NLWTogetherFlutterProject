8a536239a885e01761525aa88446c8ad7e038f1e
MahatKC <mkarvat@gmail.com>
2021-06-20 22:15:40 -0300

    app_counter finalizado

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,5 +1,45 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(app);
+  runApp(
+    MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    print(count);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu Primeiro App"),
+      ),
+      body: Center(
+          child: Text(
+        "Contador\n$count",
+        textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
 }
