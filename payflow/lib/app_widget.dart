92fb165d72da1b6764647f2ece855df8c31e82c5
MahatKC <mkarvat@gmail.com>
2021-06-24 10:56:48 -0300

    barcode scanner page

--- payflow/lib/app_widget.dart
+++ payflow/lib/app_widget.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'modules/login/login_page.dart';
 
 class AppWidget extends StatelessWidget {
+
+  AppWidget(){
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(
+        primarySwatch: Colors.orange,
         primaryColor: AppColors.primary,
       ),
       initialRoute: "/splash",
@@ -17,6 +28,7 @@ class AppWidget extends StatelessWidget {
         "/splash": (context) => SplashPage(),
         "/home": (context) => HomePage(),
         "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
       },
     );
   }
